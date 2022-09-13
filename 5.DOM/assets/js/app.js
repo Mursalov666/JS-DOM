a099533213c21b489ec1fd236da210da240d4073
Ragif <rafigrm@code.edu.az>
2022-09-14 01:29:25 +0400

    DOM

---
+++ 5.DOM/assets/js/app.js
@@ -0,0 +1,20 @@
+var btn_add = document.getElementById('btn_add');
+
+
+btn_add.onclick = function() {
+    var value = document.getElementById('input').value;
+    var h3 = document.createElement('h3');
+    var icon = document.createElement('i');
+    var div = document.createElement('div');
+    div.setAttribute('class','box')
+    icon.setAttribute('class','fa-solid fa-trash')
+    h3.innerHTML= value
+    var todo = document.getElementById('todos');
+    div.appendChild(h3)
+    div.appendChild(icon)
+    todo.appendChild(div)
+    icon.addEventListener("click", (e) => {
+        var parent = e.target.parentElement;
+        parent.remove();
+    })
+}
